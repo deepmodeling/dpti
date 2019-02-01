20185edb9b417ff080c87f0b3a44fd0ae79ae8bd
Han Wang <wang_han@iapcm.ac.cn>
2019-02-01 13:35:27 +0800

    fix bugs

--- equi.py
+++ equi.py
@@ -315,8 +315,8 @@ def _print_thermo_info(info, more_head = '') :
     ptr += '# Pzz     [bar]:  %20.8f %20.8f\n' % (info['pzz'], info['pzz_err'])
     ptr += '# Pxy     [bar]:  %20.8f %20.8f\n' % (info['pxy'], info['pxy_err'])
     ptr += '# Pxz     [bar]:  %20.8f %20.8f\n' % (info['pxz'], info['pxz_err'])
-    ptr += '# Pyz     [bar]:  %20.8f %20.8f' % (info['pyz'], info['pyz_err'])
-    prt += '# water density [kg/m^3] : %10.5f\n' % (18 * 1e-3 / (info['v'] * pc.Avogadro * pc.angstrom**3))
+    ptr += '# Pyz     [bar]:  %20.8f %20.8f\n' % (info['pyz'], info['pyz_err'])
+    ptr += '# water density [kg/m^3] : %10.5f' % (18 * 1e-3 / (info['v'] * pc.Avogadro * pc.angstrom**3))
     print(ptr)
 
 def post_task(iter_name, natoms = None, is_water = True) :
