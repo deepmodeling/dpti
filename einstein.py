1cae0f90745fa00e03b40c093e89444c0e353d02
Han Wang <wang_han@iapcm.ac.cn>
2019-02-15 08:40:03 +0800

    add changes

--- einstein.py
+++ einstein.py
@@ -52,9 +52,15 @@ def free_energy (job) :
     assert(os.path.isfile(equi_conf))
     equi_conf = os.path.abspath(equi_conf)
     os.chdir(cwd)
-    spring_k = jdata['spring_k']
     temp = jdata['temp']
     mass_map = jdata['model_mass_map']
+    spring_k = jdata['spring_k']
+    if type(spring_k) is not list:
+        spring_k_1 = []
+        for ii in mass_map :
+            spring_k_1.append(spring_k)
+        spring_k = spring_k_1
+    assert(len(mass_map) == len(spring_k))
     if 'copies' in jdata :
         ncopies = np.prod(jdata['copies'])
     else :
@@ -77,17 +83,77 @@ def free_energy (job) :
     print('# fixed atom of type %d ' % first_type)
 
     fe = 0
+    fact = pc.Boltzmann * temp / pc.electron_volt / np.sum(natoms)
     for idx,ii in enumerate(natoms) :
         # kinetic contrib
+        # print(idx)
         fe += 3 * ii * np.log(Lambda_k[idx])
+        # print(3 * ii * np.log(Lambda_k[idx]) * fact)
         if (idx == first_type) :            
             fe += 3 * (ii-1) * np.log(Lambda_s[idx])
             fe += np.log(ii / (vol * (pc.angstrom**3)))
+            # print(3.0 * (ii-1) * np.log(Lambda_s[idx]) * fact)
+            # print(np.log(ii / (vol * (pc.angstrom**3))) * fact)
         else :
             fe += 3 * ii * np.log(Lambda_s[idx])
+            # print(3.0 * ii * np.log(Lambda_s[idx]) * fact)
+    fe *= pc.Boltzmann * temp / pc.electron_volt
+    fe /= np.sum(natoms)
+    # print(fe)
+    return fe
+
+
+def frenkel(job) :
+    jdata = json.load(open(os.path.join(job, 'in.json'), 'r'))    
+    equi_conf = jdata['equi_conf']
+    cwd = os.getcwd()
+    os.chdir(job)
+    assert(os.path.isfile(equi_conf))
+    equi_conf = os.path.abspath(equi_conf)
+    os.chdir(cwd)
+    temp = jdata['temp']
+    mass_map = jdata['model_mass_map']
+    s_spring_k = jdata['spring_k']
+    spring_k = jdata['spring_k']
+    assert(type(spring_k) is not list)
+    if type(spring_k) is not list:
+        spring_k_1 = []
+        for ii in mass_map :
+            spring_k_1.append(spring_k * ii)
+        spring_k = spring_k_1
+    if 'copies' in jdata :
+        ncopies = np.prod(jdata['copies'])
+    else :
+        ncopies = 1    
+
+    sys_data = lib.lmp.to_system_data(open(equi_conf).read().split('\n'))
+    vol = np.linalg.det(sys_data['cell'])
+    natoms = [ii * ncopies for ii in sys_data['atom_numbs']]
+
+    Lambda_k = [compute_lambda(temp, ii) for ii in mass_map]
+    Lambda_s = [compute_spring(temp, ii) for ii in spring_k]
+    s_Lambda_s = compute_spring(temp, s_spring_k)
+
+    fe = 0
+    sum_m = 0
+    fact = pc.Boltzmann * temp / pc.electron_volt / np.sum(natoms)
+    for idx,ii in enumerate(natoms) :
+        fe += 3.0 * ii * np.log(Lambda_k[idx])
+        fe += 3.0 * ii * np.log(Lambda_s[idx])
+        # print(idx)
+        # print(3.0 * ii * np.log(Lambda_k[idx]) * fact)
+        # print(3.0 * ii * np.log(Lambda_s[idx]) * fact)
+        sum_m += mass_map[idx] * ii
+    fe -= 3.0 * np.log(s_Lambda_s)
+    fe -= 1.5 * np.log(sum_m)
+    # print((3.0 * np.log(s_Lambda_s) + 1.5 * np.log(sum_m)) * fact)
+    fe += np.log(np.sum(natoms) / (vol * (pc.angstrom**3)))    
+    # print(np.log(np.sum(natoms) / (vol * (pc.angstrom**3))) * fact, np.log(np.sum(natoms) / 3.0 / (vol * (pc.angstrom**3))) * fact)
+
     fe *= pc.Boltzmann * temp / pc.electron_volt
     fe /= np.sum(natoms)
     return fe
+        
     
 def _main() :
     parser = argparse.ArgumentParser(
--- hti_ice.py
+++ hti_ice.py
@@ -90,7 +90,10 @@ def _main ():
         # printing
         print_format = '%20.12f  %10.3e  %10.3e'
         hti.print_thermo_info(thermo_info)
-        print('# free ener of Einstein Mole: %20.8f' % (e0))
+        if crystal == 'vega' :
+            print('# free ener of Einstein Mole: %20.8f' % (e0))
+        else :
+            print('# free ener of Einstein Crys: %20.8f' % (e0))
         print('# Pauling corr:               %20.8f' % (pauling_corr))
         print(('# fe integration              ' + print_format) \
               % (de, de_err[0], de_err[1]))        
