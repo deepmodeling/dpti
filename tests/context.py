4fd2b0029faf23d8aacea371d46eee660d222069
felix5572 <757627927@qq.com>
2021-03-05 22:22:25 +0800

    add fake graph.pb

--- tests/context.py
+++ tests/context.py
@@ -1,3 +1,7 @@
-import sys,os
+import sys,os,hashlib,pathlib
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import deepti
+
+
+def get_file_md5(file_path):
+    return hashlib.md5(pathlib.Path(file_path).read_bytes()).hexdigest()
