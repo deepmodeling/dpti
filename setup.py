86e167d529c2712943e29a3a11a9a1e0ddca57c0
felix5572 <757627927@qq.com>
2021-05-23 16:10:10 +0800

    update setup.py for dargs

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ today = datetime.date.today().strftime("%b-%d-%Y")
 # with open(path.join(NAME, '_date.py'), 'w') as fp :
 #     fp.write('date = \'%s\'' % today)
 
-install_requires=['apache-airflow>2.0', 'scipy', 'numpy', 'pymbar']
+install_requires=['apache-airflow>2.0', 'scipy', 'numpy', 'pymbar', 'dargs']
 
 setuptools.setup(
     name='dpti',
