ca64639c41ab7d2a7f500fae11d2faffa838f12b
felix5572 <757627927@qq.com>
2021-05-25 18:25:51 +0800

    gdi.py airflow main will fail when sub dag fail

--- dpti/dags/dp_ti_gdi.py
+++ dpti/dags/dp_ti_gdi.py
@@ -8,11 +8,12 @@ from airflow.decorators import task
 from airflow.utils.dates import days_ago
 from airflow.utils.task_group import TaskGroup
 from datetime import datetime, timedelta
-from airflow.exceptions import AirflowFailException, AirflowSkipException, DagNotFound, DagRunAlreadyExists
+from airflow.exceptions import AirflowSkipException, DagNotFound, DagRunAlreadyExists, DagRunNotFound, AirflowException,  AirflowFailException
 # from airflow.api.client.local_client import Client
+from airflow.utils.state import State
 
 from airflow.api.client.local_client import Client
-from airflow.models import Variable, dagrun
+from airflow.models import Variable, DagRun
 from numpy.core.fromnumeric import var
 # from dpdispatcher.
 from dpdispatcher.submission import Submission, Task
@@ -62,6 +63,7 @@ class GDIDAGFactory:
             prepare_return = True
             return prepare_return
         
+        # @task()
         @task(retries=2, retry_delay=timedelta(minutes=1))
         def dpti_gdi_main_loop(prepare_return, **kwargs):
             # context = get_current_context()
@@ -113,7 +115,7 @@ class GDIDAGFactory:
     def create_loop_dag(self):
         @task(multiple_outputs=True)
         def dpti_gdi_loop_prepare():
-            Variable.set(self.var_name, 'run')
+            # Variable.set(self.var_name, 'run')
 
             context = get_current_context()
             dag_run = context['params']
@@ -126,7 +128,8 @@ class GDIDAGFactory:
             return {'task0_dict':task0_dict, 'task1_dict':task1_dict}
             # return (task0_dict, task1_dict)
 
-        @task()
+        # @task()
+        @task(retries=2, retry_delay=timedelta(minutes=1))
         def dpti_gdi_loop_md(task_dict):
             context = get_current_context()
             dag_run = context['params']
@@ -151,7 +154,7 @@ class GDIDAGFactory:
         @task()
         def dpti_gdi_loop_end(task0_return, task1_return):
             end_return = True
-            Variable.set(self.var_name, 'end')
+            # Variable.set(self.var_name, 'end')
             return end_return
 
         loop_dag = DAG(self.dag_loop_name, **self.__class__.dagargs)
@@ -159,7 +162,6 @@ class GDIDAGFactory:
             tasks_dict = dpti_gdi_loop_prepare()
             task0_return = dpti_gdi_loop_md(tasks_dict['task0_dict'])
             task1_return = dpti_gdi_loop_md(tasks_dict['task1_dict'])
-            # end_return = dpti_gdi_loop_end(task0_return, task1_return)
             end_return = dpti_gdi_loop_end(task0_return, task1_return)
         return loop_dag
 
@@ -168,37 +170,49 @@ class GDIWorkflow:
     def __init__(self, dag_name, var_name):
         self.dag_name = dag_name
         self.var_name = var_name
+        self.run_id = None
+
+    def get_dag_run_state(self):
+        if self.run_id is None:
+            raise DagRunNotFound(f"dag_id {self.dag_name}; {self.run_id}")
+
+        dag_runs = DagRun.find(
+            dag_id=self.dag_name,
+            run_id=self.run_id
+        )
+        
+        return dag_runs[0].state if dag_runs else None
 
     def wait_until_end(self):
-        var_value = Variable.get(self.var_name)
-        # var_value = None
-        print('begin wait until end; var_value', var_value)
         while True:
-            var_value = Variable.get(self.var_name)
-            if var_value == 'end':
-                print('wait finish', var_value)
+            dag_run_state = self.get_dag_run_state()
+            if dag_run_state == State.SUCCESS:
+                print(f"dag_run_state: {dag_run_state}")
                 break
-            else:
-                print('wait continue', var_value)
+            elif dag_run_state == State.RUNNING:
+                print(f"dag_run_state: {dag_run_state}")
                 time.sleep(30)
-        return var_value
+            else:
+                raise AirflowFailException(f"subdag dag_run fail dag_id:{self.dag_name}; run_id:{self.run_id};")
+        return dag_run_state
 
     def trigger_loop(self, submission, task_list, mdata):
         # loop_num = None
         c = Client(None, None)
         submission_dict = submission.serialize()
-        # task0_dict = task0.serialize()
-        # task1_dict = task1.serialize()
         task_dict_list = [task.serialize() for task in task_list]
         submission_hash = submission.submission_hash
-        # submission_hash = submission.submission_hash
+        self.run_id = f"dag_run_{submission_hash}"
         try:
-            c.trigger_dag(dag_id=self.dag_name, run_id=f"dag_run_{submission_hash}",
+            c.trigger_dag(dag_id=self.dag_name, run_id=self.run_id,
                 conf={'submission_dict': submission_dict, 'task_dict_list':task_dict_list, 'mdata':mdata}
-            ) #, conf={'loop_num': loop_num})
-            Variable.set(self.var_name, 'begin')
+            ) 
         except DagRunAlreadyExists:
-            print('continue from old dagrun')
+            dag_run_state = self.get_dag_run_state()
+            if dag_run_state == State.FAILED:
+                raise AirflowFailException(f"subdag dag_run fail dag_id:{self.dag_name}; run_id:{self.run_id};")
+            else:
+                print(f"continue from old dag_run {self.run_id}")
         loop_return = self.wait_until_end()
         return loop_return
 
