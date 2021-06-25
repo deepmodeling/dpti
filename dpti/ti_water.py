2fb274b0cca3febc5d974072389421cd0777874a
felix5572 <757627927@qq.com>
2021-06-25 14:34:01 +0800

    fix module import

--- dpti/ti_water.py
+++ dpti/ti_water.py
@@ -14,7 +14,7 @@ from lib.utils import get_task_file_abspath
 from lib.lammps import get_thermo
 from lib.lammps import get_natoms
 
-import dpti.ti
+from dpti import ti
 
 def _main ():
     parser = argparse.ArgumentParser(
