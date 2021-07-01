3341a1468d98e3b31a18d5112a8388e6e384656d
Yuan Fengbo <yuanfengbo888@pku.edu.cn>
2021-07-01 20:28:55 +0800

    fix bug; will add element_num

--- dpti/hti.py
+++ dpti/hti.py
@@ -573,6 +573,8 @@ def _make_tasks(iter_name, jdata, ref, switch = 'one-step', step = 'both', link 
             sparam['sigma_1_1'] = sparam['sigma_hh']
 
         element_num = len(mass_map)
+        sparam['element_num'] = element_num
+
         sigma_key_index = filter(lambda t:t[0] <= t[1], ((i,j) for i in range(element_num) for j in range(element_num)))
         sigma_key_name_list = ['sigma_'+str(t[0])+'_'+str(t[1]) for t in sigma_key_index ]
         for sigma_key_name in sigma_key_name_list:
