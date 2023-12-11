0bbf2fe1382f88c8a0b2eeb0547d13c4ea66e2ec
Yifan Li <yifanl0716@gmail.com>
2023-12-11 18:46:01 -0500

    fix the other test_ti_gen_lammps_input thermo_modify format error

--- tests/test_ti_gen_lammps_input.py
+++ tests/test_ti_gen_lammps_input.py
@@ -58,7 +58,7 @@ class TestTiGenLammpsInput(unittest.TestCase):
         thermo          ${THERMO_FREQ}
         compute         allmsd all msd
         thermo_style    custom step ke pe etotal enthalpy temp press vol c_allmsd[*]
-        thermo_modify   4*8 format %20.6f
+        thermo_modify   format 4*8 %20.6f
         # dump            1 all custom ${DUMP_FREQ} traj.dump id type x y z
         fix             1 all npt temp ${TEMP} ${TEMP} ${TAU_T} iso ${PRES} ${PRES} ${TAU_P}
         fix             mzero all momentum 10 linear 1 1 1
