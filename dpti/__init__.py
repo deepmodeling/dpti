da0ac126ee89dcc7211d1c52af103c1d352b7726
Yifan Li李一帆 <yifanl0716@gmail.com>
2023-12-13 23:46:32 -0500

    replace import modules with __all__

--- dpti/__init__.py
+++ dpti/__init__.py
@@ -6,4 +6,4 @@ SHORT_CMD = "dpti"
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import dpti
 
-from . import equi, gdi, hti, hti_liq, lib, ti
+__all__ = ["equi", "gdi", "hti", "hti_liq", "hti_ice", "hti_water", "ti", "ti_water", "lib"]
