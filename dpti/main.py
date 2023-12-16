a84e9a412f1ee7053a43973f2f75c054a33358fd
Yifan Li <yifanl0716@gmail.com>
2023-12-16 15:28:12 -0500

    fix rebase conflict

--- dpti/main.py
+++ dpti/main.py
@@ -4,18 +4,9 @@ from . import equi, gdi, hti, hti_ice, hti_liq, hti_water, ti, ti_water
 
 # from . import gdi
 
-
-def main():
-    parser = argparse.ArgumentParser(
-        description="DPTI: An Automatic Workflow Software for Thermodynamic Integration Calculations"
-    )
-    main_subparsers = parser.add_subparsers(
-        title="modules",
-        description="the subcommands of dpti",
-        help="module-level help",
-        dest="module",
-        required=True,
-    )
+def create_parser():
+    parser = argparse.ArgumentParser(description="DPTI: An Automatic Workflow Software for Thermodynamic Integration Calculations")
+    main_subparsers = parser.add_subparsers(title='modules', description='the subcommands of dpti', help='module-level help', dest='module', required=True)
 
     equi.add_module_subparsers(main_subparsers)
     hti.add_module_subparsers(main_subparsers)
@@ -25,7 +16,10 @@ def main():
     ti.add_module_subparsers(main_subparsers)
     ti_water.add_module_subparsers(main_subparsers)
     gdi.add_module_subparsers(main_subparsers)
+    return parser
 
+def main():
+    parser = create_parser()
     args = parser.parse_args()
     if hasattr(args, "func"):
         args.func(args)
