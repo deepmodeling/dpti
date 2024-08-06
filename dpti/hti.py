ef1e9eabeadf296a1a3541cdaee8081a04856caa
Yifan Li <yifanl0716@gmail.com>
2024-08-06 00:31:29 -0400

    Do not set default=one-step; further clarify help message

--- dpti/hti.py
+++ dpti/hti.py
@@ -1409,7 +1409,7 @@ def hti_phase_trans_analyze(job, jdata=None):
     return if_phase_trans
 
 
-def run_task(task_dir, machine_file, task_name="one-step", no_dp=False):
+def run_task(task_dir, machine_file, task_name, no_dp=False):
     if task_name == "00" or task_name == "01" or task_name == "02":
         job_work_dir_ = glob.glob(os.path.join(task_dir, task_name + "*"))
         assert len(job_work_dir_) == 1, f"The task_name you entered is {task_name}. It indicates that you want to run tasks for step {task_name} of the two-step or three-step HTI. Please make sure that there is one and only one {task_name}.* directory in the hti task directory."
@@ -1538,7 +1538,7 @@ def add_module_subparsers(main_subparsers):
     parser_run.add_argument("JOB", type=str, help="folder of the job")
     parser_run.add_argument("machine", type=str, help="machine.json file for the job")
     parser_run.add_argument(
-        "task_name", type=str, default="one-step", help="task name, can be one-step, 00, 01, or 02"
+        "task_name", type=str, help="task name, can be one-step, 00, 01, or 02. The task names 00, 01, and 02 are used for the two-step or three-step HTI."
     )
     parser_run.add_argument(
         "--no-dp", action="store_true", help="whether to use Deep Potential or not"
