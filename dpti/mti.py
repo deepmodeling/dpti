3fe37542cf9f3415f059cd2a12db700ba9ad66d3
Yifan Li <yifanl0716@gmail.com>
2026-07-30 18:21:53 +0800

    fix: satisfy pre-commit checks

--- dpti/mti.py
+++ dpti/mti.py
@@ -353,12 +353,16 @@ def run_task(task_name, jdata, machine_file):
             os.path.join(task_name, "task.*/mass_scale_y.*/nbead.*")
         )
         link_model = (
-            f'ln -sf {shlex.quote(f"../../../{model_file}")} {shlex.quote(model_file)}' if model_file else None
+            f"ln -sf {shlex.quote(f'../../../{model_file}')} {shlex.quote(model_file)}"
+            if model_file
+            else None
         )
     elif job_type == "mass_ti":
         task_dir_list = glob.glob(os.path.join(task_name, "task.*/mass_scale_y.*"))
         link_model = (
-            f'ln -sf {shlex.quote(f"../../{model_file}")} {shlex.quote(model_file)}' if model_file else None
+            f"ln -sf {shlex.quote(f'../../{model_file}')} {shlex.quote(model_file)}"
+            if model_file
+            else None
         )
     else:
         raise RuntimeError(
